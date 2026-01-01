ebcf32a1ad5410a0e5e1bcfebe581b197d56780a
KiranFarhan786 <167645632+KiranFarhan786@users.noreply.github.com>
2026-01-01 20:45:01 +0500

    Update docusaurus.config.ts

--- book-website/docusaurus.config.ts
+++ book-website/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://your-username.github.io', // Replace with actual GitHub Pages URL
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Hackathon-Book/', // Adjust based on your repository name
+  baseUrl: '/', // Adjust based on your repository name
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
